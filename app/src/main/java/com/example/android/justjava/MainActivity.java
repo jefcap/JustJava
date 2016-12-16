9358f1b36a1fe74672cae79e514a57d6dcfffeb2
Jefferson Capovilla <jefcap@gmail.com>
2016-12-16 00:15:21 -0200

    Finished Lesson 2A - Making an App Interactive

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +20,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 5;
-        display(quantity);
         displayPrice(quantity*5);
+    }
+
+    /**
+     * This method is called when the + button is clicked.
+     */
+    public void increment(View view) {
+        quantity++;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the - button is clicked.
+     */
+    public void decrement(View view) {
+        quantity--;
+        display(quantity);
     }
 
     /**
